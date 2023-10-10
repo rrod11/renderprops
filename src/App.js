b48582a5e2b76bb9e0922d939970f00e03a92078
Roderick <Roderickj800@gmail.com>
2023-10-10 13:28:31 -0400

    got it set up

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="main-wrapper background">
       <Showcase />
-      <BaseStats />
+      <BaseStats baseStats={baseStats} />
     </div>
   );
 }
--- src/BaseStats.js
+++ src/BaseStats.js
@@ -1,8 +1,28 @@
 import "./BaseStats.css";
-function BaseStats() {
+function BaseStats({ baseStats }) {
   return (
     <div className="base-stats">
       <h1>BaseStats</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>Hit Points</td>
+            <td>{baseStats.hp}</td>
+          </tr>
+          <tr>
+            <td>Attack</td>
+            <td>{baseStats.attack}</td>
+          </tr>
+          <tr>
+            <td>Defense</td>
+            <td>{baseStats.defense}</td>
+          </tr>
+          <tr>
+            <td>Speed</td>
+            <td>{baseStats.speed}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
